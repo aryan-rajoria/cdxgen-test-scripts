04f60e2aa65ff006888fa2f0677125d8725ec20e
Aryan Rajoria <aryanrajoria1003@gmail.com>
2024-11-18 17:49:47 +0530

    add two script
    Signed-off-by: Aryan Rajoria <aryanrajoria1003@gmail.com>

---
+++ cr-test-1-command.sh
@@ -0,0 +1,16 @@
+# move into the /home/appthreat folder
+cd /home/appthreat/
+
+# Run git clone and git pull commands (replace with your actual repository)
+# git clone project
+# cd project
+# install or setup the project
+git clone https://github.com/CycloneDX/cdxgen.git 
+cd cdxgen
+
+
+# Run other commands in the container
+# ...
+
+# Exit the container shell
+exit
--- cr-test-1.sh
+++ cr-test-1.sh
@@ -15,21 +15,5 @@ docker pull ghcr.io/cyclonedx/cdxgen
 docker run -d --name "$CONTAINER_NAME" --rm -v /tmp:/tmp -p 9090:9090 -v $(pwd):/app:rw -t ghcr.io/cyclonedx/cdxgen -r /app --server --server-host 0.0.0.0
 
 # Open a shell in the container
-docker exec -it "$CONTAINER_NAME" /bin/bash
+docker exec -it "$CONTAINER_NAME" /bin/bash < cr-test-1-command.sh
 
-# move into the /home/appthreat folder
-cd /home/appthreat/
-
-# Run git clone and git pull commands (replace with your actual repository)
-# git clone project
-# cd project
-# install or setup the project
-git clone https://github.com/CycloneDX/cdxgen.git 
-cd cdxgen
-
-
-# Run other commands in the container
-# ...
-
-# Exit the container shell
-exit
